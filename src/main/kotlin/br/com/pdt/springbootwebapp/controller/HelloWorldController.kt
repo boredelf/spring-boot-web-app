96bfa85136fab77f7775e146873b5ca88a1f0ac2
eduardo <e.simao.cunha@gmail.com>
2018-12-23 15:37:11 -0200

    Added code to violate Sonar Quality Gate.

--- src/main/kotlin/br/com/pdt/springbootwebapp/controller/HelloWorldController.kt
+++ src/main/kotlin/br/com/pdt/springbootwebapp/controller/HelloWorldController.kt
@@ -10,4 +10,14 @@ class HelloWorldController {
     @GetMapping("/")
     fun index(): ResponseEntity<String> = ResponseEntity.ok("Hello world!")
 
+    fun emptyFunction() = Unit
+
+    fun nestedIfs(): Int = if ((Math.random() * 10) > 5) {
+        println("lero")
+        println("lero")
+        var lero = if (true) 1 else 3
+        lero = lero
+        if (Math.random() < 0) 2 else 3
+    } else 2
+
 }
